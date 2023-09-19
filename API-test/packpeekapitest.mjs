8da5422ff56c8e24488bc37415b13785133aaf8b
ZP <zepetzi@gmail.com>
2023-09-18 17:57:25 -0700

    working API test, creds removed

--- API-test/packpeekapitest.mjs
+++ API-test/packpeekapitest.mjs
@@ -13,7 +13,7 @@ async function run() {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
         'x-merchant-id': 'string',
-        Authorization: 'Basic ' + Buffer.from('mraIImS1waTlM1gQg94HAVsD7ze2cVTpcLLOSbKhsSj9hjMV:Dp1uCmRGQABvZRDj9yAubZ0a8Tg0wlHrR301NM02fnQG6ciWGo7qFpnF8sLufS9k').toString('base64')
+        Authorization: 'Basic ' + Buffer.from('username:pw').toString('base64')
       },
       body: new URLSearchParams(formData).toString()
     }
