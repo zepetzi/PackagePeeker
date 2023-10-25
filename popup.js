2541d4c684850a9d276a11e68e74a8d4aee66058
ZP <zepetzi@gmail.com>
2023-10-25 16:48:34 -0700

    updated fonts for body, containerized title and TN input field together, fixed both to top

---
+++ popup.js
@@ -0,0 +1,8 @@
+const trackButton = document.getElementById('trackButton');
+const trackingField = document.getElementById('trackingNumberField');
+
+trackButton.addEventListener('click', sendToLambda()) {
+
+
+
+}
