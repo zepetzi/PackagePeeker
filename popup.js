c85527466077c306b4ce87ecc5e58e9b39917efd
ZP <zepetzi@gmail.com>
2023-11-02 16:47:37 -0700

    added more functionality for post-send of payload to api, added comments

--- popup.js
+++ popup.js
@@ -3,9 +3,11 @@ const trackingField = document.getElementById('trackingNumberField');
 
 trackButton.addEventListener('click', async function sendToLambda() {
 
+    //get value inputted from input field
     const trackingInput = trackingField.value;
         
-        let gatewayResp = await fetch('<api gateway endpoint here>', {
+        //create payload for API gateway and send to endpoint
+        let gatewayResp = await fetch('https://dpknn5hqd4.execute-api.us-east-1.amazonaws.com/dev', {
             method: 'POST',
             body: JSON.stringify({"inquiryNumber":trackingInput}),
             headers: {
@@ -14,19 +16,43 @@ trackButton.addEventListener('click', async function sendToLambda() {
 
         });
 
+        //if response is successful 
         if (gatewayResp.ok) {
             let responseBody = await gatewayResp.json();
             
+            //create object for the response
             if (responseBody) {
+                /*
+                If it exists, create another object with the trackingnumber 
+                as the key and the object itself as the value
+                */
+                var trackString = responseBody["trackingNumber"];
+                var storageObj = {};
+                storageObj[trackString] = responseBody;
+                
+                //store it in local storage
+                chrome.local.storage.set(storageObj, function() {
+
+                    //if there's a storage error:
+                    if (chrome.runtime.lastError) {
+                        console.error('Local Storage Error', chrome.runtime.lastError.message);
+                    }
+                
+                });
 
             }
 
+        //otherwise if there's no response body:
         } else {
 
+            //so create variable that can be used to either display a message or information at the end?
             //create text line mentioning error
             console.error('Error:', gatewayResp.status, gatewayResp.statusText);
+
         }
 
+
+        
 
 // if carrier, send to local storage 
 
