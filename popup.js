b833bb8579784b438de061efd2941a73c07300b6
zepetzi <zepetzi@ucdavis.edu>
2023-11-06 16:54:37 -0800

    fetch seems to be working, tested storage of jsons into chrome.storage.local, ready for refactor

--- popup.js
+++ popup.js
@@ -1,84 +1,114 @@
-const trackButton = document.getElementById('trackButton');
-const trackingField = document.getElementById('trackingNumberField');
+document.addEventListener('DOMContentLoaded', (event) => {
 
-trackButton.addEventListener('click', async function sendToLambda() {
+    const trackButton = document.getElementById('trackButton');
+    const trackingField = document.getElementById('trackingNumberField');
 
-    //get value inputted from input field
-    const trackingInput = trackingField.value;
+    trackButton.addEventListener('click', async function sendToLambda() {
+
+        /*
+        ------- storage testing -------
+        let rando = Math.random();
+        let randString = rando.toString();
         
-        //create payload for API gateway and send to endpoint
-        let gatewayResp = await fetch('<api gateway endpoint here>', {
-            method: 'POST',
-            body: JSON.stringify({"inquiryNumber":trackingInput}),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-
+        localStorage.setItem([randString], 'value1');
+        console.log("added to storage")
         });
+        -------------------------------
+        */
 
-        //if response is successful 
-        if (gatewayResp.ok) {
-            let responseBody = await gatewayResp.json();
+        //get value inputted from input field
+        const trackingInput = trackingField.value;
             
-            //create object for the response
-            if (responseBody) {
-                /*
-                If it exists, create another object with the trackingnumber 
-                as the key and the object itself as the value
-                */
-                var trackString = responseBody["trackingNumber"];
-                var storageObj = {};
-                storageObj[trackString] = responseBody;
-                
-                //check for duplicates:
-                //attempt to get the string from the local storage
-                //then if it exists and is returned, set statusMessage string to
-                //indicate that user is already tracking that number
-                chrome.storage.local.get(trackString, function(returnedItem) {
-                    
-                    if (returnedItem.hasOwnProperty(trackString)) {
-                    
-                        var statusMessage = "Tracking Number Already Exists in List";
-                    
-                    } else {
-                        
-                        //otherwise, add to storage
-                        chrome.storage.local.set(storageObj, function() {
+            try {
+                //create payload for API gateway and send to endpoint
+                let gatewayResp = await fetch('<>', {
+                    method: 'POST',
+                    body: JSON.stringify({"inquiryNumber":trackingInput}),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
 
-                            //if there's a storage error:
-                            if (chrome.runtime.lastError) {
-                                console.error('Local Storage Error', chrome.runtime.lastError.message);
-                            } else {
-                                var statusMessage = "Tracking Number Added!";
-                            };
-                        });
-                    };
                 });
 
-            };
+                //if response is successful s
+                if (gatewayResp.ok) {
+                    let responseBody = await gatewayResp.json();
+                    
+                    console.log("gateway resp okay");
+                    //create object for the response
+                    if (responseBody) {
+                        /*
+                        If it exists, create another object with the trackingnumber 
+                        as the key and the object itself as the value
+                        */
 
-        //otherwise if there's no response body:
-        } else {
+                        console.log("resp body exists");
+                        var trackString = responseBody["trackingNumber"];
+                        var storageObj = {};
+                        storageObj[trackString] = responseBody;
+                        
+                        //check for duplicates:
+                        //attempt to get the string from the local storage
+                        //then if it exists and is returned, set statusMessage string to
+                        //indicate that user is already tracking that number
+                        chrome.storage.local.get(trackString, function(returnedItem) {
+                            
+                            if (returnedItem.hasOwnProperty(trackString)) {
+                                
+                                console.log("already exists");
+                                var statusMessage = "Tracking Number Already Exists in List";
+                            
+                            } else {
+                                
+                                //otherwise, add to storage
+                                chrome.storage.local.set(storageObj, function() {
 
-            //so create variable that can be used to either display a message or information at the end?
-            //create text line mentioning error
-            console.error('Error:', gatewayResp.status, gatewayResp.statusText);
+                                    console.log("added to local storage");
+                                    //if there's a storage error:
+                                    if (chrome.runtime.lastError) {
+                                        console.error('Local Storage Error', chrome.runtime.lastError.message);
 
-        }
+                                        //updateStatus("Error adding number to local storage");
+
+                                    } else {
+                                        var statusMessage = "Tracking Number Added!";
+                                    };
+                                });
+                            };
+                        });
+
+                    };
+
+                //otherwise if there's no response body:
+                } else {
+
+                    //so create variable that can be used to either display a message or information at the end?
+                    //create text line mentioning error
+                    console.error('Error:', gatewayResp.status, gatewayResp.statusText);
+                    //updateStatus("Error: No Response Body");
+
+                    }
+
+            } catch (error) {
+
+                console.error("Error")
+
+                }
+            
+            //try statement
+
+    // now (re)render visual elements?
+    // if carrier, send to local storage 
+
+    // then for every item in local storage
+
+    // depending on carrier, use the correct render function parse and sort tracking elements and populate progress bar
 
 
 
-// now (re)render visual elements?
-// if carrier, send to local storage 
 
-// then for every item in local storage
+    });
 
-// depending on carrier, use the correct render function parse and sort tracking elements and populate progress bar
+    // //upon 
 
-
-
-
-});
-
-
-//upon 
+}); //DOMContentLoaded event listener closing brackets
